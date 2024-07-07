23e5ead24ac388fb640aeb8bc6c72b4806760377
Pavan Nihal <pavannihal.c@zohocorp.com>
2024-07-07 20:46:17 +0530

    Renamed directory

--- src/main/java/com/nihal/trade/screener/TradeScreenerApplication.java
+++ src/main/java/com/nihal/trade/screener/TradeScreenerApplication.java
@@ -1,4 +1,4 @@
-package com.nihal.sample.bugtracker;
+package com.nihal.trade.screener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
